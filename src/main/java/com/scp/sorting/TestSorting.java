09ae1f5f2fcf13b15428234d0f068109f7556b1c
Dinesh <Dinesh@Dinesh>
2019-04-09 12:09:46 +0530

    DDs commit

--- src/main/java/com/scp/sorting/TestSorting.java
+++ src/main/java/com/scp/sorting/TestSorting.java
@@ -3,7 +3,6 @@ package com.scp.sorting;
 public class TestSorting {
 	public static void main(String[] args) {
 		
-		System.out.println("Print1");
 		System.out.println("Print2");
 		System.out.println("Print3");
 		System.out.println("Print4");
